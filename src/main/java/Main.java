a150d8c681d64f99f529f736451192f563032e39
chacha86 <taejincha0619@gmail.com>
2026-01-29 15:40:09 +0900

    work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,33 +5,32 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int id = 1;
-
+        int id = 0;
+        String author = "";
+        String wiseSaying = "";
         System.out.println("== 명언 앱 ==");
 
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine();
 
-            if(cmd.equals("종료")) {
+            if (cmd.equals("종료")) {
                 break;
             }
 
             if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String wiseSaying = sc.nextLine();
+                wiseSaying= sc.nextLine();
                 System.out.print("작가 : ");
-                String author = sc.nextLine();
+                author = sc.nextLine();
 
-                System.out.println(id + "번 명언이 등록되었습니다.");
                 id++;
-            }
+                System.out.println(id + "번 명언이 등록되었습니다.");
 
-            else if(cmd.equals("목록")) {
+            } else if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("2 / 작자미상 / 과거에 집착하지 마라.");
-                System.out.println("1 / 작자미상 / 현재를 사랑하라.");
+                System.out.println(id + " / " + author + " / " + wiseSaying);
             }
         }
     }
