ad75cd39a377ca3def079745a855bc5cc5e3e00e
chacha86 <taejincha0619@gmail.com>
2026-02-03 10:14:17 +0900

    9강

---
+++ src/main/java/App.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class App {
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        int lastId = 0;
+
+        WiseSaying[] wiseSayings = new WiseSaying[10];
+        int lastWiseSayingIndex = -1;
+
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+            else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+
+                WiseSaying wiseSaying = new WiseSaying();
+
+                wiseSaying.id = ++lastId;
+                wiseSaying.content = content;
+                wiseSaying.author = author;
+
+                wiseSayings[++lastWiseSayingIndex] = wiseSaying;
+                System.out.println(lastId + "번 명언이 등록되었습니다.");
+
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = lastWiseSayingIndex; i >= 0; i--) {
+                    WiseSaying foundedWiseSaying = wiseSayings[i];
+                    System.out.println(foundedWiseSaying.id + " / " + foundedWiseSaying.author + " / " + foundedWiseSaying.content);
+                }
+            }
+        }
+    }
+}
