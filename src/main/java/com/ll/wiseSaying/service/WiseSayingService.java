3a3779c5cdfb0a3f17511e971b89efb781b8ff71
chacha86 <taejincha0619@gmail.com>
2026-02-04 14:51:54 +0900

    28

--- src/main/java/com/ll/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/ll/wiseSaying/service/WiseSayingService.java
@@ -1,44 +1,20 @@
 package com.ll.wiseSaying.service;
 
 import com.ll.wiseSaying.entity.WiseSaying;
+import com.ll.wiseSaying.repository.WiseSayingRepository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class WiseSayingService {
 
-    private List<WiseSaying> wiseSayings = new ArrayList<>();
-    private int lastId = 0;
+    WiseSayingRepository wiseSayingRepository = new WiseSayingRepository();
 
     public WiseSaying findById(int id) {
-
-        int foundedIndex = findIndexById(id);
-
-        if (foundedIndex == -1) {
-            return null;
-        }
-
-        return wiseSayings.get(foundedIndex);
-    }
-
-    private int findIndexById(int id) {
-        return IntStream
-                .range(0, wiseSayings.size())
-                .filter((i) -> wiseSayings.get(i).getId() == id)
-                .findFirst()
-                .orElse(-1);
+        return wiseSayingRepository.findById(id);
     }
 
     public List<WiseSaying> findList() {
-
-        List<WiseSaying> foundedWiseSayings = new ArrayList<>();
-
-        for (WiseSaying wiseSaying : wiseSayings.reversed()) {
-            foundedWiseSayings.add(wiseSaying);
-        }
-
-        return foundedWiseSayings;
+        return wiseSayingRepository.findList();
     }
 
     public void modify(WiseSaying wiseSaying, String content, String author) {
@@ -47,13 +23,10 @@ public class WiseSayingService {
     }
 
     public boolean delete(int deleteTarget) {
-        return wiseSayings.removeIf((wiseSaying) -> wiseSaying.getId() == deleteTarget);
+        return wiseSayingRepository.delete(deleteTarget);
     }
 
     public WiseSaying write(String content, String author) {
-        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
-        wiseSayings.add(wiseSaying);
-
-        return wiseSaying;
+        return wiseSayingRepository.save(content, author);
     }
 }
